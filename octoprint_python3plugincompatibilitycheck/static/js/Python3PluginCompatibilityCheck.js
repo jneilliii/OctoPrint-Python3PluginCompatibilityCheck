0a11f3f8549b04b98be66300b34e5cca6d479d90
jneilliii <jneilliii+github@gmail.com>
2020-04-06 11:09:11 -0400

    0.1.3 Better error messages and home page validation

--- octoprint_python3plugincompatibilitycheck/static/js/Python3PluginCompatibilityCheck.js
+++ octoprint_python3plugincompatibilitycheck/static/js/Python3PluginCompatibilityCheck.js
@@ -11,9 +11,9 @@ $(function() {
 		self.settingsViewModel = parameters[0];
 		self.pluginManagerViewModel = parameters[1];
 		self.errorMessage = ko.observable(false);
-		self.checking = ko.observable(false);
+		self.checking = ko.observableArray([]);
+		self.allCompatible = ko.observable(false);
 		self.python_version = ko.observable(3.6);
-
 		self.plugins = ko.observableArray([]);
 		self.incompatible = ko.computed(function(){
 			return ko.utils.arrayFilter(self.plugins(), function(item) {
@@ -23,12 +23,13 @@ $(function() {
 
 		self.onSettingsHidden = function(){
 			self.errorMessage(false);
-			self.checking(false);
+			self.allCompatible(false);
+			self.checking([]);
 			self.plugins([]);
 		};
 
 		self.checkPlugins = function() {
-			self.checking(true);
+			self.checking.push('started');
 			$.ajax({
 				url: API_BASEURL + "plugin/python3plugincompatibilitycheck",
 				type: "GET",
@@ -37,21 +38,34 @@ $(function() {
 				contentType: "application/json; charset=UTF-8"
 			}).done(function(data){
 				self.plugins(ko.mapping.fromJS(data)());
-				ko.utils.arrayForEach(self.incompatible(), function(item){
-					var github_api_url = item.homepage().replace('https://github.com/','https://api.github.com/repos/');
-					$.ajax({
-						url: github_api_url,
-						contentType: "application/json; charset=UTF-8"
-					}).done(function(data){
-						var last_updated = moment(data.pushed_at).format('L');
-						$('#settings_plugin_python3plugincompatibilitycheck a').filter(function(){return $(this).attr('href').toLowerCase().indexOf(data.html_url.toLowerCase()) > -1}).after(' <small>(' + last_updated + ')</small>');
-					}).fail(function(){
-						self.errorMessage(true);
-					}).always(function(){
-						self.checking(false);
+				if (self.incompatible().length > 0){
+					ko.utils.arrayForEach(self.incompatible(), function(item){
+						self.checking.push(item.homepage());
+						var github_api_url = item.homepage().replace('https://github.com/','https://api.github.com/repos/');
+						if(github_api_url.indexOf('https://api.github.com/repos/') > -1) {
+							$.ajax({
+								url: github_api_url,
+								contentType: "application/json; charset=UTF-8"
+							}).done(function(data){
+								var last_updated = moment(data.pushed_at).format('L');
+								$('#settings_plugin_python3plugincompatibilitycheck a').filter(function(){return $(this).attr('href').toLowerCase().indexOf(data.html_url.toLowerCase()) > -1}).after(' <small>(' + last_updated + ')</small>');
+							}).fail(function(){
+								self.errorMessage(true);
+							}).always(function(){
+								self.checking.remove(item.homepage());
+							});
+						} else {
+							$('#settings_plugin_python3plugincompatibilitycheck a').filter(function(){return $(this).attr('href').toLowerCase().indexOf(item.homepage().toLowerCase()) > -1}).after(' <small>(Homepage not on GitHub, manual compatibility check required)</small>');
+							self.errorMessage(true);
+							self.checking.remove(item.homepage());
+						}
 					});
-				});
-		})};
+				} else {
+					self.allCompatible(true);
+				}
+			});
+			self.checking.remove('started');
+		};
 	};
 
 	OCTOPRINT_VIEWMODELS.push({
